093b9cc937b877a917298b0b5777bafe029c44a3
jazzynpro <reader_jaz@hotmail.com>
2026-05-07 21:18:50 -0500

    se agrega funcion para dibujar la cuadricula

--- serpiente.js
+++ serpiente.js
@@ -3,6 +3,8 @@
     const canvas = document.getElementById("canvasJuego");
     const ctx = canvas.getContext("2d");
 
+    const TAMANIO_CELDA = 25;
+
 
     
 
@@ -19,6 +21,36 @@
 
     function dibujarTodo() {
       limpiarCanvas();
+      //dibujarTablero();
+      dibujarTablero2();
+    }
+
+    function dibujarTablero(){
+      ctx.strokeStyle = "#ba2a2a";
+      ctx.beginPath();
+      ctx.moveTo(0,0);
+      ctx.lineTo(500,500); 
+      ctx.moveTo(0,500);
+      ctx.lineTo(500,300); 
+      ctx.moveTo(500,0);
+      ctx.lineTo(100,300);
+      ctx.stroke();
+    }
+    function dibujarTablero2(){
+      for (let i=0; i<canvas.width; i+=TAMANIO_CELDA){
+        ctx.strokeStyle = "#50df17";
+        ctx.beginPath();
+        ctx.moveTo(i,0);
+        ctx.lineTo(i,canvas.height);
+        ctx.stroke();
+      }
+      for (let i=0; i<canvas.height; i+=TAMANIO_CELDA){
+        ctx.strokeStyle = "#50df17";
+        ctx.beginPath();
+        ctx.moveTo(0,i);
+        ctx.lineTo(canvas.width,i);
+        ctx.stroke();
+      }
     }
 
 
